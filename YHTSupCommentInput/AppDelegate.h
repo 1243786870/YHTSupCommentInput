a50a6e0cbb6c18412cc19bc7af67270e0d0ad148
yanghaitao <13021003665@163.com>
2016-07-20 10:55:46 +0800

    Initial Commit

---
+++ YHTSupCommentInput/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YHTSupCommentInput
+//
+//  Created by yanghaitao  on 16/7/20.
+//  Copyright © 2016年 yanghaitao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YHTSupCommentInput/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YHTSupCommentInput
+//
+//  Created by yanghaitao  on 16/7/20.
+//  Copyright © 2016年 yanghaitao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
